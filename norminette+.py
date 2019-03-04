bb5059772a135603ccfd03d2d714f20513ce75dd
Thijs de Jong <hi@thijsdejong.me>
2019-03-04 14:07:51 +0100

    First working version

---
+++ norminette+.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+import os
+
+def get_files():
+	files = [f for f in os.listdir('.') if os.path.isfile(f)]
+	return (files)
+
+
+def double_operation(line):
+	operations = 0
+
+	if ("=" in line):
+		operations += 1
+	if ("++" in line):
+		operations += 1
+	if ("--" in line):
+		operations += 1
+	if ("if (" in line):
+		operations += 1
+	if ("while (" in line):
+		operations += 1
+
+	if (operations > 1):
+		return (1)
+	return (0)
+
+def check_file(filename):
+	print("Norme: " + filename)
+	if (filename.endswith(".c")):
+		file = open(filename, "r")
+		ln = 1
+		for line in file:
+			if (double_operation(line)):
+				print("line " + str(ln) + ": multiple operations")
+			ln += 1
+
+def main():
+	files = get_files()
+	for file in files:
+		check_file(file)
+
+if (__name__ == "__main__"):
+	main()
