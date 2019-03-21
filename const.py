a70a61c3bd5c3dc83b0db2c1a1c1f34627e19d9c
Thijs de Jong <tde-jong@student.codam.nl>
2019-03-21 12:06:05 +0100

    remove : as operator

--- const.py
+++ const.py
@@ -1,9 +1,9 @@
 """ Holds all the constants """
 # Operations
 OPERATIONS = (' = ', '++', '--', '+=', '-=', '*=', '/=', '&=', '|=', '^=', '<<=', '>>=', 'if (',
-              'while (', 'return (', '?', ':')
+              'while (', 'return (', '?')
 
 # Version management
-VERSION = '19.3.10'
+VERSION = '19.3.11'
 VERSIONFILE = "https://raw.githubusercontent.com/thijsdejong/codam-norminette-plus/master/version"
 UPDATECOMMAND = "git -C ~/norminette+ pull"
