044661bc907c409d1d3d57739b9316cc1a9a13cc
Jorn Brinksma <44733663+jbrinksma@users.noreply.github.com>
2019-06-21 13:09:40 +0200

    improve norminette+ through regex (#15)
    * add recursion and behave like norminette
    
    * makes norminette+ more intelligent
    
    * improves intelligence of of norminette+ through the power of regex
    
    * removes old norminette+ draft
    
    * fixed pylint error
    
    * removes trailing whitespaces
    
    * Update version
    
    * Update version
    
    * Update const.py
    
    * Update version
    
    * Update const.py
    
    * sets .py permissions to 644

--- checks/cfiles.py
+++ checks/cfiles.py
@@ -1,28 +1,74 @@
 """ Contains the checks for C files """
-from const import OPERATIONS
+import re
+from const import RE_OPERATIONS
 
-def is_comment(line):
-    """ Checks if a line is a comment """
-    return (line.startswith('/*') or
-            line.startswith('**') or
-            line.startswith('*/'))
 
-def check_multiple_operations(f):
+def remove_singleline_comments(f_content):
+    """ gets rid of singleline comments // """
+    f_content = re.sub(r'\/\/.*', " ", f_content)
+
+    return f_content
+
+
+def remove_multiline_comments(f_content):
+    """ gets rid of multiline comments /*   */ """
+    re_matches = re.findall(r'\/\*[\s\S]*?\*\/', f_content)
+    f_content = remove_re_matches(f_content, re_matches)
+
+    return f_content
+
+
+def remove_strings(f_content):
+    """ gets rid of strings in code """
+    re_matches = re.findall(r'"[\s\S]*?"', f_content)
+    f_content = remove_re_matches(f_content, re_matches)
+
+    return f_content
+
+
+def remove_re_matches(f_content, re_matches):
+    """ removes the re_matches except the newlines """
+    replacer = " "
+
+    for match in re_matches:
+        newlines = 0
+        for char in match:
+            if char == "\n":
+                newlines += 1
+        while newlines > 0:
+            replacer += "\n"
+            newlines -= 1
+        f_content = f_content.replace(match, replacer)
+
+    return f_content
+
+
+def remove_comments_and_strings(f_content):
+    """ gets rid interfering comments and strings """
+    f_content = remove_multiline_comments(f_content)
+    f_content = remove_singleline_comments(f_content)
+    f_content = remove_strings(f_content)
+
+    return f_content
+
+
+def check_multiple_operations(f_content):
     """ Checks for double operations if the line is not a comment """
     l = 1
-    for line in f:
+    for line in f_content:
         operations = 0
-        if not is_comment(line):
-            for operation in OPERATIONS:
-                if operation in line:
-                    operations += 1
-                    if operations > 1:
-                        print "Error (line " + str(l) + "): multiple operations"
+        for operation in RE_OPERATIONS:
+            operations += len(re.findall(operation, line))
+        if operations > 1:
+            print "Error (line " + str(l) + "): multiple operations"
         l += 1
-    f.seek(0)
+
 
 def check(filename):
     """ Loops through all lines """
     print "Norme: " + filename
     f = open(filename, "r")
-    check_multiple_operations(f)
+    f_content = f.read()
+    f_content = remove_comments_and_strings(f_content)
+    f_content = f_content.split("\n")
+    check_multiple_operations(f_content)
--- const.py
+++ const.py
@@ -1,7 +1,27 @@
 """ Holds all the constants """
 # Operations
-OPERATIONS = (' = ', '++', '--', '+=', '-=', '*=', '/=', '%=', '&=', '|=', '^=', '<<=', '>>=',
-              'if (', 'while (', 'return (')
+
+                 # checks for all single '=' related matches e.g: |=, +=, %=
+RE_OPERATIONS = (r'(?<=[\+\-\*\/\%\|\&\^\w\d\s])=(?=[^=])',
+
+
+                 # Ternary checking will be released when
+                 # Thijs fixes his weird ternary operations in 42sh.
+                 # Otherwise, this addition would fail all Travis'
+                 # builds :)
+                 # filename: parser_debug: line: 27, line: 42
+
+                 # matches ternary operators
+                 # r'\?(?=[^=])',
+
+
+                 r'\+\+',
+                 r'--',
+                 r'<<=',
+                 r'>>=',
+                 r'if \(',
+                 r'while \(',
+                 r'return \(')
 MAKEFILENAMES = ("GNUmakefile", "makefile", "Makefile")
 MAKEFILERULES = ("all:", "$(NAME):", "clean:", "fclean:", "re:")
 
