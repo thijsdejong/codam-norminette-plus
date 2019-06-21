ddc307a1bd5cd6360a76fcc77613e7c7210232cc
Thijs de Jong <hi@thijsdejong.me>
2019-06-21 15:01:35 +0200

    fix quicknorm on mac

--- quicknorm.sh
+++ quicknorm.sh
@@ -6,7 +6,7 @@ TARGET_DIR="/Users/$USER/goinfre/quicknorm_temp_"
 
 # because we are using a potentially dangerous rm -rf, we randomize 16
 # chars in the directory name which we use and remove.
-RAND_STR=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 16 | head -n 1)
+RAND_STR=$(cat /dev/urandom | LC_CTYPE=C tr -dc 'a-zA-Z0-9' | fold -w 16 | head -n 1)
 
 TARGET_DIR+=$RAND_STR
 
