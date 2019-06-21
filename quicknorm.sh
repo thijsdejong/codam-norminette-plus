a9de23012cc7dd3738e5762648191726839d54bb
Thijs de Jong <hi@thijsdejong.me>
2019-06-21 15:03:11 +0200

    Make quicknorm silent

--- quicknorm.sh
+++ quicknorm.sh
@@ -10,4 +10,4 @@ RAND_STR=$(cat /dev/urandom | LC_CTYPE=C tr -dc 'a-zA-Z0-9' | fold -w 16 | head 
 
 TARGET_DIR+=$RAND_STR
 
-git clone $REPO $TARGET_DIR && python $TARGET_DIR/run.py; rm -rf $TARGET_DIR
+git clone -q $REPO $TARGET_DIR && python $TARGET_DIR/run.py; rm -rf $TARGET_DIR
