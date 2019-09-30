d1ca3c59bc0cd776d3ed4e361b163bcc6ebf3c76
Thijs de Jong <tde-jong@student.codam.nl>
2019-09-30 11:49:57 +0200

    19.9.2

--- const.py
+++ const.py
@@ -26,6 +26,6 @@ MAKEFILENAMES = ("GNUmakefile", "makefile", "Makefile")
 MAKEFILERULES = ("all:", "$(NAME):", "clean:", "fclean:", "re:")
 
 # Version management
-VERSION = '19.9.1'
+VERSION = '19.9.2'
 VERSIONFILE = "https://raw.githubusercontent.com/thijsdejong/codam-norminette-plus/master/version"
 UPDATECOMMAND = "norminette+ --update"
